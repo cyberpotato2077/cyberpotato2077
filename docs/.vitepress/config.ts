c5c93dde44280ac6c97fb278b5f2fd767fe4cc20
cyberpotato2077 <cyberpotato96@gmail.com>
2025-04-20 00:21:20 +0900

    홈 꾸미기, 기본적인 페이지 정리

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,29 +1,38 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig } from 'vitepress';
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "My Awesome Project",
-  description: "A VitePress Site",
+  title: '말하는 감자',
+  description: '어쩌다 만든 블로그입니다.',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: 'About Me', link: '/me' },
+      // { text: 'Examples', link: '/markdown-examples' },
     ],
 
     sidebar: [
       {
-        text: 'Examples',
+        text: '감자 블로그',
         items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
+          { text: 'About Me', link: '/me' },
+          { text: '블로그 만드는 법', link: '/guide/how-to-make-vitepress' },
+        ],
+      },
+      {
+        text: 'Examples (Deprecated)',
+        items: [
+          { text: 'Markdown Examples', link: '/example/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/example/api-examples' },
+        ],
+      },
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
+      // https://simpleicons.org/
+      { icon: 'github', link: 'https://github.com/cyberpotato2077' },
+    ],
   },
-  base:'/cyberpotato2077/'
-})
+  base: '/cyberpotato2077/',
+});
